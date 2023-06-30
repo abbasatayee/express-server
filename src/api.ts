a94438c73ffd37577457249bfff3184689198e7b
Abbas Ataie <49268657+abbasatayee@users.noreply.github.com>
2023-06-30 11:43:13 +0430

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -11,13 +11,13 @@ app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 'okkkkkk' });
+  res.status(200).send({ status: 'Custom push' });
 });
 
 const api = express.Router();
 
 api.get('/hello', (req, res) => {
-  res.status(200).send({ message: 'hello world' });
+  res.status(200).send({ message: 'hello world from cloudstation' });
 });
 
 // Version the api
